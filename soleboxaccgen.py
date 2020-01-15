cbabec591b68a71740c6dae170c2d2add0b7c277
rtuna <38296319+rtunaboss@users.noreply.github.com>
2020-01-15 16:58:37 +0100

    fixed creating account issues

--- soleboxaccgen.py
+++ soleboxaccgen.py
@@ -256,53 +256,16 @@ if country_id == None:
     quit()
 
 
-
 headers = {
-    # 'authority': 'www.solebox.com',
-    # 'method': 'GET',
-    # 'path': '/',
-    # 'scheme': 'https',
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-    'Accept-Encoding': 'gzip, deflate, br',
-    'Accept-Language': 'en-us',
-    'Connection': 'keep-alive',
-    # 'cache-control': 'no-cache',
-    # 'referer': 'http://www.solebox.com/',
-    # 'sec-fetch-mode': 'navigate',
-    # 'sec-fetch-site': 'cross-site',
-    # 'upgrade-insecure-requests': '1',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'accept-encoding': 'gzip, deflate, br',
+    'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8,cs;q=0.7,de;q=0.6',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-site': 'none',
+    'sec-fetch-user': '?1',
+    'upgrade-insecure-requests': '1',
+    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36',
 }
-
-
-
-# headers = {
-#     # 'authority': 'www.solebox.com',
-#     # 'method': 'GET',
-#     # 'path': '/',
-#     # 'scheme': 'https',
-#     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-#     'accept-encoding': 'gzip, deflate',
-#     'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8,cs;q=0.7,de;q=0.6',
-#     # 'cache-control': 'no-cache',
-#     # 'sec-fetch-mode': 'navigate',
-#     'Connection': 'keep-alive',
-#     # 'sec-fetch-site': 'none',
-#     'upgrade-insecure-requests': '1',
-#     'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.79 Safari/537.36',
-# }
-
-# :method: GET
-# :scheme: https
-# :authority: www.solebox.com
-# :path: /mein-konto/
-# Cookie: cto_bundle=vmAr8F9ianNlb1E1VWp5a3hSNTY5QWV3SkxjVFpMZVZMTEpoY3BHaWFvdmx3NSUyQktqdk94aHlHNllXU2tZV3BiejhSdFNCTHVvTmVRT25Ub0xtenVSSDNVbjJ6RmY4Qll4UEFPNVpHMHMlMkI4dFA2a1FNJTJCMmp2UXJBN0x3VFliQVByeEFZbw; _px3=9014804bb6db7c387f7e5d0ffe5cd08880e3f9d5cae56e7871b1484d9e0c286e:rnBMvmhhOnRtTqb6UInpqUJaIa72Z+TT09RJyjPow2DBhXtNg4+DI8AMigmFp7+cWb2KO5IdxJE2pQY0Z8JgWg==:1000:vE+U1gmTOT1uwX7ZOucNgyhUntNrh1IwuW1puaZliq1R6jgpAEKFsZ0nStDBwbDzHRy7AC8mhQljic+jK6M0u5cf5PfpEhwwyEAIAk8UBMqHqNx9JzOv9J5swyCrF70J51l2fS33Lfpnh8bkSZV9sBcxqCAcgsHQV7o4r3PjNkA=; __utma=1.1099094876.1576941685.1576941685.1576941685.1; __utmb=1.2.10.1576941685; __utmc=1; __utmz=1.1576941685.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); px-abgroup=A; px-abper=100; language=0; _fbp=fb.1.1576941685612.724889629; _pxvid=8d430e2f-2405-11ea-ac46-0242ac12000a; cto_lwid=54e30eff-8413-44ae-8c4f-ac1166ee2436; __utmt=1; displayedCookiesNotification=1; mlid=undefined
-# Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8
-# Accept-Encoding: gzip, deflate, br
-# Host: www.solebox.com
-# User-Agent: Mozilla/5.0 (iPhone; CPU iPhone OS 12_1_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.0 Mobile/15E148 Safari/604.1
-# Accept-Language: en-us
-# Referer: https://www.solebox.com/
-# Connection: keep-alive
 
 linetwolist = ['apt', 'apartment', 'dorm', 'suite', 'unit', 'house', 'unt', 'room', 'floor']
 
@@ -311,7 +274,7 @@ def generateAccount():
     ##########     Initializing a session & getting stoken     ##########
     useragent = random.choice(useragents)
     headers['user-agent'] = useragent
-    # headers['user-agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36'
+    # headers['User-Agent'] = useragent
     with logger.print_lock:
         print(gettime() + ' [STATUS] -> Account generation has started...')
     s = cfscrape.create_scraper()
@@ -322,7 +285,9 @@ def generateAccount():
             s.proxies = random.choice(proxyList)
             with logger.print_lock:
                 print(gettime() + ' [STATUS] -> Checking proxy...')
-            test = s.get('https://www.solebox.com/', headers=headers)
+            
+            t = s.get('https://www.solebox.com/en/home/', headers=headers)
+            test = s.get('https://www.solebox.com/en/open-account/', headers=headers)
             if test.status_code in (302, 200):
                 with logger.print_lock:
                     print(Fore.GREEN + Style.BRIGHT + gettime() + ' [SUCCESS] -> Proxy working...')
@@ -361,64 +326,70 @@ def generateAccount():
     with logger.print_lock:
         print(gettime() + ' [STATUS] -> Trying to create an account...')
     ##########     Configuring payload for registering and POSTing it to create an account     ##########
+
     # register_payload = {
-    #         'stoken': stoken,
-    #         'lang': 1,
-    #         'listtype': '',
-    #         'actcontrol': 'register',
-    #         'fnc': 'registeruser',
-    #         'cl': 'register',
-    #         'lgn_cook' : 0,
-    #         'reloadaddress': '',
-    #         'blshowshipaddress': 1,
-    #         'option' : 3,
-    #         'invadr[oxuser__oxsal]': random.choice(['MR', 'MRS']),  # MR OR MRS
-    #         'invadr[oxuser__oxfname]': firstName,
-    #         'invadr[oxuser__oxlname]': lastName,
-    #         'invadr[oxuser__oxstreet]': jiggedFirstLineAddress,
-    #         'invadr[oxuser__oxstreetnr]': houseNum,
-    #         'invadr[oxuser__oxaddinfo]': jiggedSecondLineAddress,
-    #         'invadr[oxuser__oxzip]': zipcode,
-    #         'invadr[oxuser__oxcity]': city,
-    #         'invadr[oxuser__oxcountryid]': country_id,
-    #         'invadr[oxuser__oxstateid]': stateUS,
-    #         'invadr[oxuser__oxbirthdate][day]': random.randint(1, 31),
-    #         'invadr[oxuser__oxbirthdate][month]': random.randint(1, 12),
-    #         'invadr[oxuser__oxbirthdate][year]': random.randint(1950, 2003),
-    #         'invadr[oxuser__oxfon]': phoneNum,
-    #         'lgn_usr': email,
-    #         'lgn_pwd': passwd,
-    #         'lgn_pwd2': passwd,
+    #     'stoken': stoken,
+    #     'lang': 1,
+    #     'listtype': '',
+    #     'actcontrol': 'account',
+    #     'cl': 'user',
+    #     'fnc': 'createuser',
+    #     'reloadaddress': '',
+    #     'blshowshipaddress': 1,
+        # 'invadr[oxuser__oxsal]': random.choice(['MR', 'MRS']),  # MR OR MRS
+        # 'invadr[oxuser__oxfname]': firstName,
+        # 'invadr[oxuser__oxlname]': lastName,
+        # 'invadr[oxuser__oxstreet]': jiggedFirstLineAddress,
+        # 'invadr[oxuser__oxstreetnr]': houseNum,
+    #     'invadr[oxuser__oxaddinfo]': jiggedSecondLineAddress,
+        # 'invadr[oxuser__oxzip]': zipcode,
+        # 'invadr[oxuser__oxcity]': city,
+        # 'invadr[oxuser__oxcountryid]': country_id,
+        # 'invadr[oxuser__oxstateid]': stateUS,
+        # 'invadr[oxuser__oxbirthdate][day]': random.randint(1, 31),
+        # 'invadr[oxuser__oxbirthdate][month]': random.randint(1, 12),
+        # 'invadr[oxuser__oxbirthdate][year]': random.randint(1950, 2003),
+    #     'invadr[oxuser__oxfon]': phoneNum,
+    #     'lgn_usr': email,
+    #     'lgn_pwd': passwd,
+    #     'lgn_pwd2': passwd,
     #     }
 
     register_payload = {
         'stoken': stoken,
-        'lang': 1,
-        'listtype': '',
-        'actcontrol': 'account',
-        'cl': 'user',
-        'fnc': 'createuser',
+        'lang': '1',
+        'actcontrol': 'register',
+        'fnc': 'registeruser',
+        'cl': 'register',
+        'lgn_cook': '0',
         'reloadaddress': '',
-        'blshowshipaddress': 1,
-        'invadr[oxuser__oxsal]': random.choice(['MR', 'MRS']),  # MR OR MRS
-        'invadr[oxuser__oxfname]': firstName,
-        'invadr[oxuser__oxlname]': lastName,
-        'invadr[oxuser__oxstreet]': jiggedFirstLineAddress,
-        'invadr[oxuser__oxstreetnr]': houseNum,
-        'invadr[oxuser__oxaddinfo]': jiggedSecondLineAddress,
-        'invadr[oxuser__oxzip]': zipcode,
-        'invadr[oxuser__oxcity]': city,
-        'invadr[oxuser__oxcountryid]': country_id,
-        'invadr[oxuser__oxstateid]': stateUS,
-        'invadr[oxuser__oxbirthdate][day]': random.randint(1, 31),
-        'invadr[oxuser__oxbirthdate][month]': random.randint(1, 12),
-        'invadr[oxuser__oxbirthdate][year]': random.randint(1950, 2003),
-        'invadr[oxuser__oxfon]': phoneNum,
+        'option': '3',
         'lgn_usr': email,
         'lgn_pwd': passwd,
         'lgn_pwd2': passwd,
-        }
+        'blnewssubscribed': '0',
+        'invadr[oxuser__oxsal]': random.choice(['MR', 'MRS']),  # MR OR MRS
+        'invadr[oxuser__oxfname]': firstName,
+        'invadr[oxuser__oxlname]': lastName,
+        'invadr[oxuser__oxcompany]': '',
+        'invadr[oxuser__oxaddinfo]': jiggedSecondLineAddress,
+        'invadr[oxuser__oxstreet]': jiggedFirstLineAddress,
+        'invadr[oxuser__oxstreetnr]': houseNum,
+        'invadr[oxuser__oxzip]': zipcode,
+        'invadr[oxuser__oxcity]': city,
+        'invadr[oxuser__oxustid]': '',
+        'invadr[oxuser__oxcountryid]': country_id,
+        'invadr[oxuser__oxstateid]': stateUS,
+        'invadr[oxuser__oxfon]': phoneNum,
+        'invadr[oxuser__oxfax]': '',
+        'invadr[oxuser__oxmobfon]': '',
+        'invadr[oxuser__oxprivfon]': '',
+        'invadr[oxuser__oxbirthdate][day]': random.randint(1, 31),
+        'invadr[oxuser__oxbirthdate][month]': random.randint(1, 12),
+        'invadr[oxuser__oxbirthdate][year]': random.randint(1950, 2003),
+    }
 
+    headers['Referer'] = 'https://www.solebox.com/en/open-account/'
     register_post = s.post(url='https://www.solebox.com/index.php?lang=1&', headers=headers, data=register_payload)
     if register_post.status_code in (302, 200):
         with logger.print_lock:
